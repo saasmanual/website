5bd200f615fc8f14eab401a689252c33721a0a0b
Nikolai Onken <nikolai@nikolaionken.com>
2020-12-17 18:08:12 +0100

    Add sample tools (#3)
    * Add sample tools
    
    * Add other types.
    
    * Add support for redirects.
    
    * Move tools.

--- infra/stack/website.js
+++ infra/stack/website.js
@@ -1,10 +1,14 @@
-const { join } = require('path');
+const { join, relative } = require('path');
 const { Stack, Duration } = require('@aws-cdk/core');
 const { PublicHostedZone, RecordTarget, ARecord } = require('@aws-cdk/aws-route53');
 const { CloudFrontTarget } = require('@aws-cdk/aws-route53-targets');
 const { PriceClass, CloudFrontWebDistribution, OriginProtocolPolicy } = require('@aws-cdk/aws-cloudfront');
-const { Bucket, BucketAccessControl } = require('@aws-cdk/aws-s3');
+const { Bucket, BucketAccessControl, RedirectProtocol, ReplaceKey } = require('@aws-cdk/aws-s3');
 const { BucketDeployment, Source, CacheControl } = require('@aws-cdk/aws-s3-deployment');
+const { readFileSync } = require('fs');
+const glob = require('glob');
+
+const DEPRECATED_PATH = join(__dirname, '..', '..', 'src', 'deprecated');
 
 class WebsiteStack extends Stack {
 
@@ -20,12 +24,31 @@ class WebsiteStack extends Stack {
       zoneName
     });
 
+    const files = glob.sync(join(DEPRECATED_PATH, '**/*'));
+    
+    const redirectRules = files.map((path) => {
+      const redirectFrom = relative(DEPRECATED_PATH, path);
+      const redirectTo = readFileSync(path).toString();
+
+      return {
+        hostName: 'saasmanual.com',
+        httpRedirectCode: '301',
+        protocol: RedirectProtocol.HTTPS,
+        replaceKey: ReplaceKey.with(redirectTo),
+        condition: {
+          keyPrefixEquals: redirectFrom,
+        }
+      }
+    });
+    
+
     const websiteAssets = new Bucket(this, `${id}-website-production-assets`, {
       accessControl: BucketAccessControl.PUBLIC_READ,
       bucketName: `saas-manual-website-prod-static-assets`,
       publicReadAccess: true,
       websiteIndexDocument: 'index.html',
-      websiteErrorDocument: '404/index.html'
+      websiteErrorDocument: '404/index.html',
+      websiteRoutingRules: redirectRules
     });
 
     new BucketDeployment(this, 'DeployWebsite', {
---
+++ src/content/tools/cloudflare/index.md
@@ -0,0 +1,5 @@
+---
+draft: true
+keywords: cloudflare
+layout: default.njk
+---
