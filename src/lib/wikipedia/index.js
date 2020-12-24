fa720ba1ad3f2a1cc98e81e3241fc160d7ba00d5
Nikolai Onken <nikolai@nikolaionken.com>
2020-12-24 14:09:51 +0100

    Use module to split sentences.

--- src/lib/wikipedia/index.js
+++ src/lib/wikipedia/index.js
@@ -1,6 +1,7 @@
 import visit from 'unist-util-visit';
 import h from 'hastscript';
 import fetch from 'node-fetch';
+import { split } from "sentence-splitter";
 
 const WIKIPEDIA_URL = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&inprop=url&pageids=`;
 
@@ -11,22 +12,26 @@ async function getWikipediaContent(node) {
   const json = await response.json();
 
   const { extract, title } = json.query.pages[id];
-  const excerpt = extract.split('. ')[0] + '. ';
+  const excerpt = split(extract)[0].raw;
 
   const data = node.data || (node.data = {});
-  
+
   const hast = h('div', {
     className: ``
   }, [
-    h('h3', 'From Wikipedia'), 
-    { 
-      type: 'text', 
-      value:  excerpt 
-    }, 
+    h('h3', 'From Wikipedia'),
+    {
+      type: 'text',
+      value: excerpt
+    },
+    {
+      type: 'html',
+      value: '&nbsp;'
+    },
     {
       type: 'element',
       tagName: 'small',
-      children: [{ 
+      children: [{
         type: 'element',
         tagName: 'a',
         properties: {
