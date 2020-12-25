cef92e5af1a91f1563bd67a547b9151e02c874c2
Nikolai Onken <nikolai@nikolaionken.com>
2020-12-25 14:18:21 +0100

    Add canonical tag.

--- src/lib/meta/index.js
+++ src/lib/meta/index.js
@@ -1,9 +1,11 @@
 import { dirname } from 'path';
+import blogConfig from '../../../config.json';
 
 function meta(baseline) {
   for (const file in baseline.ctx) {
     const props = baseline.ctx[file];
-    props.data.link = `/${dirname(file)}`;
+    const dir = dirname(file);
+    props.data.link = `${blogConfig.url}${dir === '.' ? '/' : `/${dir}`}`;
   }
 }
 
