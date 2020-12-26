fc5f59aaf54a90aca8d6d2e7cc454e7f657c4648
Nikolai Onken <nikolai@nikolaionken.com>
2020-12-26 14:22:38 +0100

    Render table for SEO plugin.

--- src/lib/seo/index.js
+++ src/lib/seo/index.js
@@ -1,26 +1,37 @@
-import debug from 'debug';
+import { table, getBorderCharacters } from 'table';
+import chalk from 'chalk';
 
-const log = debug('markdown-seo');
+const TABLE_CONFIG = {
+  border: getBorderCharacters(`norc`),
+  drawHorizontalLine: (index, size) => {
+    return index === 0 || index === 1 || index === size;
+  }
+};
 
 function validateDescription(file, description) {
   const MIN_LENGTH = 55;
   const MAX_LENGTH = 300;
   
   if (!description) {
-    log('Please provide a description for:', file);
-    return;
+    return `${chalk.red('✘')} (Required)`;
   }
   
   if (description.length < MIN_LENGTH || description.length > MAX_LENGTH) {
-    log(`Please provide a description between ${MIN_LENGTH} and ${MAX_LENGTH} characters:`, file)
-    return;
+    return `${chalk.red('✘')} (Between ${MIN_LENGTH} and ${MAX_LENGTH} chars.)`;
   }
+
+  return chalk.green(`✔`);
 }
 
 async function seo(generator) {
+  const data = [[chalk.bold('File'), chalk.bold('Description')]];
+
   for (const file in generator.ctx) {
-    validateDescription(file, generator.ctx[file].data.description);
+    data.push([file, validateDescription(file, generator.ctx[file].data.description)]);
   }
+  
+  console.log(table(data, TABLE_CONFIG));
+
   return this;
 }
 
