bdf9ffa52c28551310730aa5a3af39141faf672f
Nikolai Onken <nikolai@nikolaionken.com>
2021-01-03 15:20:46 +0100

    (fix: website) Generate Changelog manually for now.
    CodePipeline doesn't clone the Git history, so I am not able to generate the Changelog as part of the build step. For now, I am manually generating it locally. Eventually, this should happen automatically.

--- src/lib/changelog/index.js
+++ src/lib/changelog/index.js
@@ -1,7 +1,7 @@
-
-const { parseCommits, gitFormat } = require('./helper');
-const { spawn } = require('child_process');
-
+import { parseCommits, gitFormat } from './helper';
+import { spawn } from 'child_process';
+import { writeFileSync, readFileSync } from 'fs';
+import { join } from 'path';
 
 async function changelog(generator) {
   const source = spawn('git', ['log', gitFormat]);
@@ -9,8 +9,15 @@ async function changelog(generator) {
   source.on('error', function (err) {
     console.log(`Something went wrong: ${err}`);
   });
-  
-  const commits = await parseCommits(source.stdout);
+
+  let commits;
+  if (process.env.NODE_ENV === 'production') {
+    commits = JSON.parse(readFileSync(join(__dirname, 'commits.json'), 'utf-8'));
+  } else {
+    commits = await parseCommits(source.stdout);
+    writeFileSync(join(__dirname, 'commits.json'), JSON.stringify(commits), 'utf-8');
+  }
+
 
   for (const file in generator.ctx) {
     const props = generator.ctx[file];
